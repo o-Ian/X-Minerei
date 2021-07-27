7accc5b6c0596c23d185a02eb9329f27f076a509
Ian Stigliano Silva <ianstigli@hotmail.com>
2021-07-27 18:19:27 -0300

    CSV to SQL

---
+++ config.py
@@ -0,0 +1,6 @@
+config = {
+    'user': 'root', # nome do Usuário do Banco
+    'host': 'localhost', # nome do Host do Banco
+    'password': '', # senha do Banco
+    'database': 'xminerei'# nome do banco de dados
+}
