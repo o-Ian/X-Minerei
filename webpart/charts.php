0c20d4297934b13e6a8319fb7a46801e37ec99a0
Ian Stigliano Silva <ianstigli@hotmail.com>
2021-10-15 02:37:16 -0300

    testing

--- webpart/charts.php
+++ webpart/charts.php
@@ -8,7 +8,7 @@ include_once('ur_logged.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gráficos - X-Minerei</title>
+    <title>TESTE - X-Minerei</title>
     <link rel="stylesheet" href="css/charts.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700;800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
