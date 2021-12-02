cdd9d49a1cda957784c49c11dd3745a4c45a5a0a
Ian Stigliano Silva <ianstigli@hotmail.com>
2021-12-02 16:08:08 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,17 @@
 <?php
 session_start();
-    $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'xminerei';
+    $dbHost = 'us-cdbr-east-04.cleardb.com';
+    $dbUsername = 'b2a2784d1d4897';
+    $dbPassword = '2a178fe4';
+    $dbName = 'heroku_e79c9ad2ac8e9f9';
     
     $conection = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 
-    mysqli_query($conection, "USE xminerei;");
+    if (mysqli_ping($conection)) {
+        printf ("Our connection is ok!\n");
+    } else {
+        printf ("Error: %s\n", mysqli_error($link));
+    }
 
    /* if($conection->connect_errno){
         echo "Erro";
