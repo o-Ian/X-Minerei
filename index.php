755bae6070a1dc275efed8b9504ff4fb7ed2707d
Ian Stigliano Silva <ianstigli@hotmail.com>
2021-12-02 17:40:50 -0300

    Teste deploy automático

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>X-Minerei</title>
+    <title>X-Minerei TESTE DEPLOY AUTOMÁTICO</title>
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/menu.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
